241d78b5b704dacfe74775a45dddc4c52e224fe4
EenMacFullstack <Iain.mcdulling@gmail.com>
2021-11-18 19:24:22 +0000

    Initial setup created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logos/notflix-img.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <head>
+        <title>Notflix Carousel Intro</title>
+      </head>
+
+      <body>
+        
+        <div>
+          <img src = {logo} class = "logo"/>
+          <h1 class = "introduction-reminder">Friendly Reminder to have fun</h1>
+        </div>
+
+        <div class="carouselbox">
+          <div class="carousel">
+
+          </div>
+
+          
+          <a class= "switch-left">left switch</a>
+          <a class= "switch-right">right switch</a>
+
+        </div>
+
+
+      </body>
+
+      <foot>
+
+      </foot>
+      
     </div>
   );
 }
